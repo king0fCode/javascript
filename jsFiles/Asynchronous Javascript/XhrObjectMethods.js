5c9c42f172a371bd20ecf24adb2a3956acbdfac6
king0fCode <mr.robot.code.94@gmail.com>
2018-10-21 17:50:39 +0530

    3rd change

---
+++ jsFiles/Asynchronous Javascript/XhrObjectMethods.js
@@ -0,0 +1,39 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+    // create XHR object
+    const xhr = new XMLHttpRequest();
+    // open
+    xhr.open('GET', 'data.txt', true);
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(this.responseText);
+            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+        }
+    }
+    // optional  - used for spinners / loaders
+    // xhr.onprogress = function(){
+    //   // processing state put spinner or loader 
+    // }
+    //   
+    xhr.onerror = function() {
+        console.log('request error');
+    }
+    // old methods to check ready state //
+    // xhr.onreadystatechange = function() {
+    //     if (this.status === 200 && this.readyState === 4) {
+    //         console.log(this.responseText);
+    //     }
+    // }
+    // http statues
+    // 200 ok 
+    // 403: forbieed
+    // 404: not found
+    // readyState values
+    // 0 : request not initialized
+    //1 : server connection established
+    //2: request received 
+    //3: processing request 
+    //4: request fnished and response is ready
+    xhr.send();
+}
--- jsFiles/Asynchronous Javascript/ajaxAndJson.js
+++ jsFiles/Asynchronous Javascript/ajaxAndJson.js
@@ -3,13 +3,10 @@ document.getElementById('button2').addEventListener('click', loadCustomers);
 
 function loadCustomer(e) {
     const xhr = new XMLHttpRequest();
-
     xhr.open('GET', 'customer.json', true);
-
     xhr.onload = function() {
         if (this.status === 200) {
             console.log(this.responseText);
-
             const user = JSON.parse(this.responseText);
             const output = `
 <ul>
@@ -19,29 +16,20 @@ function loadCustomer(e) {
 <li>ID ${user.phone}</li>
 
 </ul>`;
-
             document.getElementById('customer').innerHTML = output;
         }
     }
-
     xhr.send();
 }
-
-
 // Load customers 
-
 function loadCustomers(e) {
     const xhr = new XMLHttpRequest();
-
     xhr.open('GET', 'customers.json', true);
-
     xhr.onload = function() {
         if (this.status === 200) {
             console.log(this.responseText);
-
             const users = JSON.parse(this.responseText);
             let output = '';
-
             users.forEach(function(user) {
                 output += `
 <ul>
@@ -52,15 +40,9 @@ function loadCustomers(e) {
 
 </ul>`;
             });
-
-
-
-
             document.getElementById('customer').innerHTML = output;
         }
     }
-
     xhr.send();
 }
-
 // THIS is replaced object name inside the function
