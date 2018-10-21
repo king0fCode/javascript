270eb1f312429da369a776bc7e9f44d6a163a4cc
king0fCode <mr.robot.code.94@gmail.com>
2018-10-21 17:21:08 +0530

    second change

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
 document.getElementById('button').addEventListener('click', loadData);
 
 function loadData() {
-  // Create an XHR Object
-  const xhr = new XMLHttpRequest();
+    // create XHR object
+    const xhr = new XMLHttpRequest();
 
-  // OPEN
-  xhr.open('GET', 'data.txt', true);
+    // open
+    xhr.open('GET', 'data.txt', true);
 
-  // console.log('READYSTATE', xhr.readyState);
 
-  // Optional - Used for spinners/loaders
-  xhr.onprogress = function(){
-    console.log('READYSTATE', xhr.readyState);
-  }
 
-  xhr.onload = function(){
-    console.log('READYSTATE', xhr.readyState);
-    if(this.status === 200) {
-      // console.log(this.responseText);
-      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(this.responseText);
+            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+        }
     }
-  }
 
-  // xhr.onreadystatechange = function() {
-  //   console.log('READYSTATE', xhr.readyState);
-  //   if(this.status === 200 && this.readyState === 4){
-  //     console.log(this.responseText);
-  //   }
-  // }
-
-  xhr.onerror = function() {
-    console.log('Request error...');
-  }
-
-  xhr.send();
+    // optional  - used for spinners / loaders
+    // xhr.onprogress = function(){
+    //   // processing state put spinner or loader 
+    // }
+    //   
 
 
-    // readyState Values
-    // 0: request not initialized 
-    // 1: server connection established
-    // 2: request received 
-    // 3: processing request 
-    // 4: request finished and response is ready
+
+    xhr.onerror = function() {
+        console.log('request error');
+
+    }
 
 
-  // HTTP Statuses
-  // 200: "OK"
-  // 403: "Forbidden"
-  // 404: "Not Found"
+
+
+    // old methods to check ready state //
+    // xhr.onreadystatechange = function() {
+    //     if (this.status === 200 && this.readyState === 4) {
+    //         console.log(this.responseText);
+
+    //     }
+
+    // }
+
+    // http statues
+    // 200 ok 
+    // 403: forbieed
+    // 404: not found
+
+    // readyState values
+    // 0 : request not initialized
+    //1 : server connection established
+    //2: request received 
+    //3: processing request 
+    //4: request fnished and response is ready
+
+
+
+    xhr.send();
+
 }
